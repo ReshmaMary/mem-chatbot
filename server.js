42f12e2148463fe11da4f41fd64462d4276747b9
ReshmaMary <reshmamary.serine@gmail.com>
2018-09-17 22:17:22 -0500

    make it better

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.get('/', function(req, res){
-    res.sendfile('./index.html');
+app.get('*', function(req, res){
+    res.sendfile('.dist/index.html');
 });
 // Start the app by listening on the default
 // Heroku port
