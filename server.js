84b241d71d4ce364982f03425561fc70dc4fd859
ReshmaMary <reshmamary.serine@gmail.com>
2018-09-18 11:42:17 -0500

    make it better

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.get('*',function(req, res){
     //res.sendFile(path.join(__dirname + '/dist/MEMChatbot/index.html'));
    // res.sendfile(_dirname+'/dist/MEMChatbot/index.html');
 });
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  // Instruct the app
+  // to use the forceSSL
+  // middleware
+  app.use(forceSSL());
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
